f3239cc8578aba2481dca53e462cb3268c1a5292
Fokoda799 <fey2003@gmail.com>
2025-08-15 16:11:28 +0100

    fix: remove trailing slash from Socket.IO server URL

--- frontend/src/helpers/socket.ts
+++ frontend/src/helpers/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
 // Connect to backend Socket.IO server
-export const socket = io("https://inktrail.onrender.com/", { autoConnect: false });
+export const socket = io("https://inktrail.onrender.com", { autoConnect: false });
